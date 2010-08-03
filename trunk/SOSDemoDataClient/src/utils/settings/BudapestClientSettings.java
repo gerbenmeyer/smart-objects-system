35e126b6f1b255c8217eb71004f04e349e4e217e
gerben.meyer <gerben.meyer@6957567c-6bed-0201-62d5-a081d24f62d1>
2010-08-03 14:34:11 +0000

    Initial import of SOSDemoDataClient.

---
+++ trunk/SOSDemoDataClient/src/utils/settings/BudapestClientSettings.java
@@ -0,0 +1,10 @@
+package utils.settings;
+
+
+public class BudapestClientSettings {
+	
+	public final static String SERVER_ADDRESS = "localhost";
+	public final static int SERVER_PORT = 3458;
+	public final static String PASSWORD = "adminpw";
+	public final static String USERNAME = "DemoClient";
+}
