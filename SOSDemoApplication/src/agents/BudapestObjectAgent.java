556e44b0dd28890faa583bcace6e8223bf9eb701
gerben.meyer <gerben.meyer@6957567c-6bed-0201-62d5-a081d24f62d1>
2010-08-03 14:33:14 +0000

    Initial import of SOSDemoApplication.

---
+++ SOSDemoApplication/src/agents/BudapestObjectAgent.java
@@ -0,0 +1,67 @@
+package agents;
+
+import java.util.HashMap;
+
+import model.agent.Agent;
+import model.agent.collection.AgentCollectionView;
+import model.agent.property.properties.LocationProperty;
+import util.htmltool.HtmlDetailsPaneContentGenerator;
+import util.htmltool.HtmlMapContentGenerator;
+import util.htmltool.HtmlTool;
+
+/**
+ * 
+ * @author Gerben G. Meyer
+ */
+public class BudapestObjectAgent extends Agent {
+
+	public BudapestObjectAgent(String id, AgentCollectionView pocv) {
+		super(id, pocv);
+	}
+
+	public void act() throws Exception {
+		//no acting needed for this object
+	}
+
+	public void lastWish() {
+		//no last wish needed for this object
+	}
+
+	/**
+	 * @return the garbage
+	 */
+	public boolean isGarbage() {
+		//this agent is never garbage
+		return false;
+	}
+	
+	@Override
+	public void generateMapContent(HtmlMapContentGenerator mapContent, HashMap<String, String> params) {
+		//check whether the used link is a deeplink to this object
+		boolean deeplink = params != null && params.containsKey("deeplink");
+		if (deeplink) {
+			//in case of a deeplink, add all objects to the map as the home-agent does
+			getAgentCollectionView().get("home").generateMapContent(mapContent, params);
+		}
+		
+		//pan the map to the location of this object
+		LocationProperty lp = new LocationProperty("", getLocation());
+		if (!lp.isNull()) {
+			mapContent.panToLocation(lp.getLatitude(), lp.getLongitude());
+			mapContent.setZoom(14);
+		}
+		
+		//show the info window of this object
+		mapContent.popupInfoWindow(this.getID());
+
+	}
+	
+	@Override
+	public void generateDetailsPaneContent(HtmlDetailsPaneContentGenerator detailsPane, HashMap<String, String> params) {
+		//generate the details pane in the same way as the default agent does that
+		detailsPane.addHeader(HtmlTool.createImage(getIcon(), getType(), 16) + " " + getLabel());
+		
+		detailsPane.addParagraph(getDescription());
+	}
+
+}
