e29f26e048069cbbb70348a13ee86e5af1d5e887
wilrik <wilrik@6957567c-6bed-0201-62d5-a081d24f62d1>
2010-11-29 15:55:19 +0000

    - changed agents to create their own balloon content using the new generator

--- SOSBudapestApplication/src/agents/BudapestObjectAgent.java
+++ SOSBudapestApplication/src/agents/BudapestObjectAgent.java
@@ -2,14 +2,12 @@ package agents;
 
 import java.util.HashMap;
 
-import main.Settings;
 import model.agent.Agent;
 import model.agent.AgentViewable;
 import model.agent.collection.AgentCollection;
 import model.agent.property.properties.LocationProperty;
 import util.htmltool.HtmlDetailsPaneContentGenerator;
 import util.htmltool.HtmlMapContentGenerator;
-import util.htmltool.HtmlTool;
 
 /**
  * 
@@ -65,18 +63,5 @@ public class BudapestObjectAgent extends Agent {
 		AgentViewable av = AgentCollection.getInstance().get("search");
 		params.put("q", "type:" + get(Agent.TYPE));
 		av.generateDetailsPaneContent(detailsPane, params);
-	}
-
-	@Override
-	public String createMapBalloonContent() {
-		String infoWindowContent = "";
-		if (Boolean.parseBoolean(Settings.getProperty(Settings.SHOW_AGENT_DETAILS))) {
-			HashMap<String, String> infoWindowContentAttriutes = new HashMap<String, String>();
-			infoWindowContentAttriutes.put("class", "infoWindowContent");
-			infoWindowContent += HtmlTool.createLink(getID()+".html", HtmlTool.createImage(getIcon(), getID())+get(Agent.LABEL), "hidden_frame")
-				+ HtmlTool.createLink("?" + Settings.getProperty(Settings.KEYWORD_DEEPLINK) + "=" + getID(), HtmlTool.createImage("link.png", "deeplink to "+getID()))
-				+ HtmlTool.createDiv(get(Agent.DESCRIPTION), infoWindowContentAttriutes);
-		}
-		return infoWindowContent;
 	}
 }
