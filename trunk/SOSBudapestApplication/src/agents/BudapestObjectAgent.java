b199eab870cee6d9047b26ee3b95582a95a2f691
gerben.meyer@gmail.com <gerben.meyer@gmail.com@6957567c-6bed-0201-62d5-a081d24f62d1>
2010-12-06 12:16:55 +0000

    Changes to Budapest application reflecting changes to SOSGeneric.

--- trunk/SOSBudapestApplication/src/agents/BudapestObjectAgent.java
+++ trunk/SOSBudapestApplication/src/agents/BudapestObjectAgent.java
@@ -3,10 +3,8 @@ package agents;
 import java.util.HashMap;
 
 import model.agent.Agent;
-import model.agent.AgentViewable;
-import model.agent.collection.AgentCollection;
 import model.agent.property.properties.LocationProperty;
-import util.htmltool.HtmlDetailsPaneContentGenerator;
+import util.htmltool.HtmlMapBalloonContentGenerator;
 import util.htmltool.HtmlMapContentGenerator;
 
 /**
@@ -17,6 +15,7 @@ public class BudapestObjectAgent extends Agent {
 
 	public BudapestObjectAgent(String id) {
 		super(id);
+		setNeedsDetailsPane(false);
 	}
 
 	public void act() throws Exception {
@@ -37,31 +36,19 @@ public class BudapestObjectAgent extends Agent {
 	
 	@Override
 	public void generateMapContent(HtmlMapContentGenerator mapContent, HashMap<String, String> params) {
-		//check whether the used link is a deeplink to this object
-		boolean deeplink = params != null && params.containsKey("deeplink");
-		if (deeplink) {
-			//in case of a deeplink, add all objects to the map as the home-agent does
-			AgentCollection.getInstance().get("home").generateMapContent(mapContent, params);
-		}
-		
 		//pan the map to the location of this object
 		LocationProperty lp = new LocationProperty("", get(Agent.LOCATION));
 		if (!lp.isNull()) {
 			mapContent.panToLocation(lp.getLatitude(), lp.getLongitude());
 			mapContent.setZoom(14);
 		}
-		
 		//show the info window of this object
 		mapContent.popupInfoWindow(this.getID());
 
 	}
 	
-	@Override
-	public void generateDetailsPaneContent(HtmlDetailsPaneContentGenerator detailsPane, HashMap<String, String> params) {
-		//show all attracctions by using the always existing search agent to search for all agents
-		detailsPane.addHeader("More " + get(Agent.TYPE) + "s");
-		AgentViewable av = AgentCollection.getInstance().get("search");
-		params.put("q", "type:" + get(Agent.TYPE));
-		av.generateDetailsPaneContent(detailsPane, params);
+	public void generateMapBalloonContent(HtmlMapBalloonContentGenerator balloonContent, HashMap<String,String> params) {
+		balloonContent.addAgentHeaderLink(this);
+		balloonContent.addParagraph(get(Agent.DESCRIPTION));
 	}
 }
