774e94ef05606bafb3df20c0703cd6d0f58d5ae4
wilrik <wilrik@6957567c-6bed-0201-62d5-a081d24f62d1>
2011-01-03 14:29:25 +0000

    added learning to the agent

--- SOSDutchWeatherApplication/src/agents/WeatherAgent.java
+++ SOSDutchWeatherApplication/src/agents/WeatherAgent.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import model.agent.Agent;
 import model.agent.property.Property;
 import model.agent.property.properties.LocationProperty;
+import util.enums.PropertyType;
 import util.htmltool.HtmlDetailsContentGenerator;
 import util.htmltool.HtmlMapBalloonContentGenerator;
 import util.htmltool.HtmlMapContentGenerator;
@@ -14,6 +15,11 @@ public class WeatherAgent extends Agent {
 
 	public WeatherAgent(String id) {
 		super(id);
+	}
+
+	@Override
+	public void act() throws Exception {
+		set(PropertyType.STATUS, Agent.STATUS, getLearnedStatus().toString());
 	}
 
 	@Override
@@ -38,24 +44,45 @@ public class WeatherAgent extends Agent {
 		// create the content of the balloon of this object, containing a header
 		// and a paragraph
 		balloonContent.addAgentHeaderLink(this);
-		balloonContent.addParagraph("Temperature: " + get("TemperatureCelcius") + " °C");
-		balloonContent.addParagraph("Wind speed: " + get("WindSpeedMS") + " m/s");
-		balloonContent.addParagraph("Wind direction: " + get("WindDirection"));
-		balloonContent.addParagraph("Rain: " + get("RainMMPerHour") + " mm/h");
+		String temp = get("TemperatureCelcius");
+		String windsp = get("WindSpeedMS");
+		String winddir = get("WindDirection");
+		String rain = get("RainMMPerHour");
+		if (!temp.isEmpty()) {
+			balloonContent.addParagraph("Temperature: " + temp + " °C");
+		}
+		if (!windsp.isEmpty()) {
+			balloonContent.addParagraph("Wind speed: " + windsp + " m/s " + winddir);
+		}
+		if (!rain.isEmpty()) {
+			balloonContent.addParagraph("Rain: " + rain + " mm/h");
+		}
 	}
 
 	@Override
 	public void generateDetailsContent(HtmlDetailsContentGenerator detailsPane, HashMap<String, String> params) {
-
 		detailsPane.addHeader(HtmlTool.createImage(getMapMarkerImage(), "") + " " + get(Agent.LABEL));
 		String time = get("Date");
 		if (!time.isEmpty()) {
 			detailsPane.addParagraph(HtmlTool.createImage("clock.png", "clock") + " " + time);
 		}
+		Property status = getProperty(Agent.STATUS);
+		detailsPane.addSubHeader("Problem detection");
+		detailsPane.addDataHeader("", "Name", "Value");
+		detailsPane.addDataRow(status.getIcon(), status.getName(), status.toInformativeString());
+		detailsPane.addSubHeader("Training");
+		detailsPane.addDataHeader("", "Training", "Status");
+		detailsPane.addDataRowTrainingButtons(getID());
+		
+		detailsPane.addSubHeader("Properties");
 		detailsPane.addDataHeader("", "Name", "Value");
 		for (String key : getPropertiesKeySet()) {
 			Property p = getProperty(key);
-			detailsPane.addDataRow(p.getIcon(), p.getName(), p.toInformativeString());			
+			if (!p.isHidden()) {
+				if (p.getPropertyType() != PropertyType.STATUS) {
+					detailsPane.addDataRow(p.getIcon(), p.getName(), p.toInformativeString());
+				}
+			}
 		}
 	}
 }
