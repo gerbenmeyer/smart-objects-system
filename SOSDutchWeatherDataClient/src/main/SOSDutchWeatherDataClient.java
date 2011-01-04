ac6f509e38b53abe5a161ab74e04adf10e85e9f6
gerben.meyer@gmail.com <gerben.meyer@gmail.com@6957567c-6bed-0201-62d5-a081d24f62d1>
2011-01-04 17:15:45 +0000

    - Various improvements to the DutchWeather demo.

--- SOSDutchWeatherDataClient/src/main/SOSDutchWeatherDataClient.java
+++ SOSDutchWeatherDataClient/src/main/SOSDutchWeatherDataClient.java
@@ -13,39 +13,49 @@ import utils.xml.WeatherXMLParser;
 public class SOSDutchWeatherDataClient {
 
 	private static RemoteAgentCollection remoteAgentCollection = new RemoteAgentCollection(
-			DutchWeatherClientSettings.SERVER_ADDRESS, DutchWeatherClientSettings.SERVER_PORT, 
+			DutchWeatherClientSettings.SERVER_ADDRESS, DutchWeatherClientSettings.SERVER_PORT,
 			DutchWeatherClientSettings.USERNAME, DutchWeatherClientSettings.PASSWORD);
 
 	private static final String XML_URL = "http://xml.buienradar.nl/";
 
 	public SOSDutchWeatherDataClient() throws FileNotFoundException {
-		// the parser
-		WeatherXMLParser parser = null;
 
-		// create new XML file parser, via http
-		try {
-			URL url = new URL(XML_URL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			parser = new WeatherXMLParser(conn.getInputStream());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		if (parser == null) {
-			throw new FileNotFoundException();
-		}
+		while (true) {
+			// the parser
+			WeatherXMLParser parser = null;
 
-		try {
-			Collection<Agent> agents = parser.parse();
-			remoteAgentCollection.connect();
-			remoteAgentCollection.put(agents);
-			remoteAgentCollection.disconnect();
-			System.out.println(agents.size()+" objects sent to server!");
-		} catch (Exception e) {
-			e.printStackTrace();
+			// create new XML file parser, via http
+			try {
+				URL url = new URL(XML_URL);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				parser = new WeatherXMLParser(conn.getInputStream());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			if (parser == null) {
+				throw new FileNotFoundException();
+			}
+
+			// sending agents to server
+			try {
+				Collection<Agent> agents = parser.parse();
+				remoteAgentCollection.connect();
+				remoteAgentCollection.put(agents);
+				remoteAgentCollection.disconnect();
+				System.out.println(agents.size() + " objects sent to server!");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				Thread.sleep(15 /*minutes*/ * 60 /*seconds*/ * 1000 /*milliseconds*/);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			new SOSDutchWeatherDataClient();
