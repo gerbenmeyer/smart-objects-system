fcc525a3179b8fb8b937b880ce4cbdcb75f23600
gerben.meyer@gmail.com <gerben.meyer@gmail.com@6957567c-6bed-0201-62d5-a081d24f62d1>
2011-01-18 14:41:49 +0000

    - Changed agent factories of demo applications, reflecting the generic changes.

--- trunk/SOSBudapestApplication/src/agents/BudapestAgentFactory.java
+++ trunk/SOSBudapestApplication/src/agents/BudapestAgentFactory.java
@@ -1,17 +1,27 @@
 package agents;
 
+import java.util.Map;
+
 import model.agent.Agent;
 import model.agent.collection.AgentFactory;
+import model.agent.property.Property;
 
 public class BudapestAgentFactory extends AgentFactory {
 
 	@Override
-	public Agent createSpecificAgent(String agentID) {
+	protected Agent createSpecificAgent(Map<String, Property> properties) {
+		String agentID = properties.get(Agent.ID).toString();
+		String agentType = "";
+		if (properties.containsKey(Agent.TYPE)){
+			agentType = properties.get(Agent.TYPE).toString();
+		}
 		if (agentID.equals("home")) {
 			return new BudapestHomeAgent(agentID);
-		} else {
+		} else if (!agentType.isEmpty()){
 			return new BudapestObjectAgent(agentID);
 		}
+		return null;
 	}
+
 
 }
