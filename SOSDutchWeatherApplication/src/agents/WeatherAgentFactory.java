c486ceb236a637bf298d0dc4b4f63dc4f2a67dfe
gerben.meyer@gmail.com <gerben.meyer@gmail.com@6957567c-6bed-0201-62d5-a081d24f62d1>
2011-01-18 14:41:49 +0000

    - Changed agent factories of demo applications, reflecting the generic changes.

--- SOSDutchWeatherApplication/src/agents/WeatherAgentFactory.java
+++ SOSDutchWeatherApplication/src/agents/WeatherAgentFactory.java
@@ -1,16 +1,27 @@
 package agents;
 
+import java.util.Map;
+
 import model.agent.Agent;
 import model.agent.collection.AgentFactory;
+import model.agent.property.Property;
 
 public class WeatherAgentFactory extends AgentFactory {
 
 	@Override
-	protected Agent createSpecificAgent(String agentID) {
+	protected Agent createSpecificAgent(Map<String, Property> properties) {
+		String agentID = properties.get(Agent.ID).toString();
+		String agentType = "";
+		if (properties.containsKey(Agent.TYPE)){
+			agentType = properties.get(Agent.TYPE).toString();
+		}
 		if (agentID.equals("home")) {
 			return new WeatherHomeAgent(agentID);
-		} else {
+		} else if (!agentType.isEmpty()){
 			return new WeatherAgent(agentID);
 		}
+		return null;
 	}
+
+
 }
